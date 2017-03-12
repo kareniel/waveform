e59d37bba4cbbb7ccb0876a299d88495bb8a76ae
Jonathan Dupré <hello@jonathandupre.com>
2017-03-12 15:14:28 -0400

    draw waveform, play file from begining of selection

--- src/Waveform.js
+++ src/Waveform.js
@@ -1,34 +1,117 @@
 const Handle = {x: null, hovered: false}
+const defaultOptions = { width: 960, height: 150 }
+const BLUE = 'rgb(2, 123, 231)'
+const GREEN = 'rgb(12, 242, 0)'
+const WHITE = 'rgb(255, 255, 255)'
 
-module.exports = function Waveform (width, height) {
+module.exports = function Waveform (url, opts = defaultOptions) {
   const _this = this
-  const el = document.createElement('canvas')
-
-  el.width = width
-  el.height = height
-  el.style.cursor = 'pointer'
-
-  this.canvas = el
-  this.ctx = el.getContext('2d')
-  this.leftHandle = Object.create(Handle)
-  this.rightHandle = Object.create(Handle)
-  this.selecting = false
-
   let x
 
-  this.canvas.addEventListener('mousedown', e => {
-    if (this.withinReach(this.leftHandle.x)) { 
-      return this.onDragMarker(this.leftHandle) 
+  this.init = () => {
+    this.loading = true
+    this.canvas = document.createElement('canvas')
+    this.ctx = this.canvas.getContext('2d')
+    this.canvas.width = opts.width
+    this.canvas.height = opts.height
+    this.canvas.style.cursor = 'pointer'
+    this.canvas.addEventListener('mousedown', e => {
+      if (this.withinReach(this.leftHandle.x)) { 
+        return this.onDragMarker(this.leftHandle) 
+      }
+      
+      if (this.withinReach(this.rightHandle.x)) { 
+        return this.onDragMarker(this.rightHandle) 
+      }
+
+      this.leftHandle.x = this.mouseX
+      this.rightHandle.x = null
+      this.onDrag(e)
+    })
+
+    this.leftHandle = Object.create(Handle)
+    this.rightHandle = Object.create(Handle)
+    this.selecting = false
+
+    this.audioCtx = new AudioContext()
+
+    this.playButton = document.createElement('button')
+    this.playButton.innerHTML = 'Play!'
+    this.playButton.addEventListener('click', e => this.play())
+
+    this.loopingButton = document.createElement('button')
+    this.loopingButton.innerHTML = 'Looping: Off'
+    this.loopingButton.disabled = true
+    this.loopingButton.addEventListener('click', e => this.toggleLooping())
+    this.looping = false
+
+    fetch(url)
+      .then(res => res.arrayBuffer())
+      .then(arrayBuffer => this.audioCtx.decodeAudioData(arrayBuffer))
+      .then(audioBuffer => {
+        this.loading = false
+        this.audioBuffer = audioBuffer
+        this.peaks = []
+        
+        const arr = audioBuffer.getChannelData(0)
+        const width = Math.ceil(arr.length / this.canvas.width)
+        const height = this.canvas.height / 2
+
+        let samples, min, max, s, pair
+
+        for (let i = 0; i < this.canvas.width; i++) {
+          samples = arr.slice(i*width, i*width+width)
+          min = samples[0]
+          max = samples[0]
+
+          for (let i = 0; i < samples.length; i++) {
+            s = samples[i]
+            max = s > max ? s : max
+            min = s < min ? s : min
+          }
+
+          pair = [(min * height) + height, (max * height) + height]
+
+          this.peaks.push(pair)  
+        }
+
+        console.log(this.peaks)
+      })
+  }
+
+  this.toggleLooping = () => {
+    if (!this.looping) {
+      this.looping = true
+      this.loopingButton.innerHTML = 'Looping: On'
+    } else {
+      this.looping = false
+      this.loopingButton.innerHTML = 'Looping: Off'
     }
-    
-    if (this.withinReach(this.rightHandle.x)) { 
-      return this.onDragMarker(this.rightHandle) 
+  }
+
+  this.pause = () => {
+    this.pausedAt = this.audioCtx.currentTime
+  }
+
+  this.play = (start) => {
+    if (this.audioSource) { 
+      this.audioSource.disconnect()
     }
 
-    this.leftHandle.x = this.mouseX
-    this.rightHandle.x = null
-    this.onDrag(e)
-  })
+    const x1 = this.leftHandle.x
+    const x2 = this.rightHandle.x
+    const total = this.audioBuffer.duration 
+
+    start = start || (this.pausedAt ? this.pausedAt : (x1 / this.canvas.width ) * total)
+    const duration = (x2 ? (x2 - x1) / this.canvas.width * total : undefined)
+
+    this.audioSource = this.audioCtx.createBufferSource()
+    this.audioSource.buffer = this.audioBuffer
+    this.audioSource.connect(this.audioCtx.destination)
+    this.audioSource.start(0, start)
+
+    this.startedAt = this.audioCtx.currentTime - start
+  }
 
   this.onDragMarker = handle => {
     document.addEventListener('mousemove', onDragMarkerMouseMove) 
@@ -58,7 +141,6 @@ module.exports = function Waveform (width, height) {
 
   this.onDrag = e => {
     this.selecting = true
-
     document.addEventListener('mousemove', onDragMouseMove) 
     document.addEventListener('mouseup', onDragMouseUp)
 
@@ -67,7 +149,7 @@ module.exports = function Waveform (width, height) {
     }
 
     function onDragMouseUp (e) {
-      this.selecting = false
+      _this.selecting = false
 
       document.removeEventListener('mousemove', onDragMouseMove)
       document.removeEventListener('mouseup', onDragMouseUp)
@@ -83,13 +165,45 @@ module.exports = function Waveform (width, height) {
     }
   }
 
+  let hasSelection
+
   this.render = () => {
     requestAnimationFrame(this.render)
 
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
 
+    if (this.loading) {
+      this.ctx.fillStyle = 'rgb(255, 255, 255)'
+      return this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+    }
+
     this.ctx.fillStyle = 'rgb(50, 50, 50)'
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+
+    hasSelection = (this.leftHandle.x && this.rightHandle.x)
+
+    /* selection */
+    // if (hasSelection) {
+    //   this.ctx.beginPath()
+    //   this.ctx.fillStyle = 'rgb(20, 20, 20)'
+    //   this.ctx.fillRect(this.leftHandle.x, 0, this.rightHandle.x - this.leftHandle.x, this.canvas.height)
+    // }
+
+    /* waveform */
+    if (this.peaks) {
+      this.ctx.lineWidth = 1
+      this.peaks.forEach((a, i) => {
+        if (hasSelection && this.betweenHandles(i)) {
+          this.ctx.strokeStyle = GREEN
+        } else {
+          this.ctx.strokeStyle = BLUE
+        }
+        this.ctx.beginPath()
+        this.ctx.moveTo(i + 1, a[0])
+        this.ctx.lineTo(i, a[1])
+        this.ctx.stroke()
+      })
+    }
 
     /* mouse cursor */
     if (this.leftHandle.hovered || this.rightHandle.hovered) {
@@ -98,17 +212,17 @@ module.exports = function Waveform (width, height) {
       this.canvas.style.cursor = 'pointer'
     }
 
-    /* selection */
-    if (this.leftHandle.x && this.rightHandle.x) {
-      this.ctx.beginPath()
-      this.ctx.fillStyle = 'rgb(20, 20, 20)'
-      this.ctx.fillRect(this.leftHandle.x, 0, this.rightHandle.x - this.leftHandle.x, this.canvas.height)
-    }
-
     /* left handle */
-    if (this.leftHandle.x) {
-      this.ctx.lineWidth = this.leftHandle.hovered ? 4 : 1
-      this.ctx.strokeStyle = this.leftHandle.hovered ? 'blue' : 'black'
+    if (this.leftHandle.x && this.leftHandle.hovered && !this.selecting) {
+      this.ctx.lineWidth = 3
+      this.ctx.strokeStyle = GREEN
+      this.ctx.beginPath()
+      this.ctx.moveTo(this.leftHandle.x, this.canvas.height)
+      this.ctx.lineTo(this.leftHandle.x, 0)
+      this.ctx.stroke()
+    } else if (!this.rightHandle.x) {
+      this.ctx.lineWidth = 1
+      this.ctx.strokeStyle = GREEN
       this.ctx.beginPath()
       this.ctx.moveTo(this.leftHandle.x, this.canvas.height)
       this.ctx.lineTo(this.leftHandle.x, 0)
@@ -116,16 +230,25 @@ module.exports = function Waveform (width, height) {
     }
 
     /* right handle */
-    if (this.rightHandle.x) {
-      this.ctx.lineWidth = this.rightHandle.hovered ? 4 : 1
-      this.ctx.strokeStyle = this.rightHandle.hovered ? 'blue' : 'black'
+    if (this.rightHandle.x && this.rightHandle.hovered && !this.selecting) {
+      this.ctx.lineWidth = 3
+      this.ctx.strokeStyle = GREEN
       this.ctx.beginPath()
       this.ctx.moveTo(this.rightHandle.x, this.canvas.height)
       this.ctx.lineTo(this.rightHandle.x, 0)
       this.ctx.stroke()
     }
 
-    /* time cursor */
+    /* current time cursor */
+    this.ctx.lineWidth = 1
+    this.ctx.strokeStyle = 'white'
+    this.ctx.beginPath()
+    this.ctx.moveTo(this.getCurrentTime() / this.audioBuffer.duration * this.canvas.width, this.canvas.height)
+    this.ctx.lineTo(this.getCurrentTime() / this.audioBuffer.duration * this.canvas.width , 0)
+    this.ctx.stroke()
+    
+
+    /* hover cursor */
     if (!this.leftHandle.hovered && !this.rightHandle.hovered) {
       this.ctx.lineWidth = 1
       this.ctx.strokeStyle = 'white'
@@ -136,8 +259,20 @@ module.exports = function Waveform (width, height) {
     }
   }
 
+  this.betweenHandles = i => {
+    if (this.leftHandle.x < this.rightHandle.x) {
+      return (i >= this.leftHandle.x && i <= this.rightHandle.x)
+    } else {
+      return (i >= this.rightHandle.x && i <= this.leftHandle.x)
+    }
+  }
+
+  this.getCurrentTime = () => {
+    return this.audioCtx.currentTime - this.startedAt
+  }
+
   this.withinReach = x => {
-    return (this.mouseX > x - 2) && (this.mouseX < x + 2)
+    return (this.mouseX > x - 3) && (this.mouseX < x + 3)
   }
 
   /* Keeps track of the mouse position on the canvas */
@@ -161,4 +296,7 @@ module.exports = function Waveform (width, height) {
 
     this.mouseX = x
   })
+
+  this.init()
+
 }
--- src/client.js
+++ src/client.js
@@ -1,8 +1,10 @@
 const yo = require('yo-yo')
 const Waveform = require('./Waveform.js')
 
-const waveform = new Waveform(800, 120)
+const waveform = new Waveform('/data/cosmicosmo-twoson.mp3')
 
 document.body.appendChild(waveform.canvas)
+document.body.appendChild(waveform.playButton)
+document.body.appendChild(waveform.loopingButton)
 
 waveform.render()
