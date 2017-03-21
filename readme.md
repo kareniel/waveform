020e4e3ecabfb6852935820b26cebfa6b3fbf83c
Jonathan Dupré <hello@jonathandupre.com>
2017-03-20 21:59:09 -0400

    install new beta v of choo. :D

---
+++ readme.md
@@ -0,0 +1,6 @@
+#waveform
+
+
+## resources
+
+- [Boris Smus's Web Audio API book][http://chimera.labs.oreilly.com/books/1234000001552/index.html]
--- src/client.js
+++ src/client.js
@@ -1,8 +1,44 @@
+const choo = require('choo')
+const html = require('bel')
+const Waveform = require('./Waveform')
+
+const wf = new Waveform('/data/cosmicosmo-twoson.mp3')
+
+const app = choo()
+
+app.use(store)
+app.route('/', layout)
+
+app.mount('body')
+
+function layout (state, emit) {
+  return html`
+    <body>
+      <nav>
+        <h1>waveform</h1>
+      </nav>
+      <div></div>
+      <section></section>
+      <section></section>
+      <section>${wf.el}</section>
+    </body>
+  `
+}
+
+function store (state, emitter) {
+  emitter.on('DOMContentLoaded', function () {
+    wf.render()
+  })
+}
+
+
+
 // const Waveform = require('./Waveform')
 // const waveform = new Waveform('/data/cosmicosmo-twoson.mp3')
+
 // document.body.appendChild(waveform.el)
 // waveform.render()
 
-const Radar = require('./Radar')
-const radar = new Radar()
-document.body.appendChild(radar.el)
+// const Radar = require('./Radar')
+// const radar = new Radar()
+// document.body.appendChild(radar.el)
