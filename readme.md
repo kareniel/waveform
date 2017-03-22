a46a62ad9c5ac6e98fbfc27f9b2f865cf2d61138
Jonathan Dupré <hello@jonathandupre.com>
2017-03-21 22:33:15 -0400

    setup basic layout for main view

--- readme.md
+++ readme.md
@@ -4,3 +4,7 @@
 ## resources
 
 - [Boris Smus's Web Audio API book][http://chimera.labs.oreilly.com/books/1234000001552/index.html]
+
+- [Seeing circles, sines and signals][https://jackschaedler.github.io/circles-sines-signals/]
+
+- [The Audio Processing Dog House][https://www.objc.io/issues/24-audio/audio-dog-house/]
--- src/client.js
+++ src/client.js
@@ -1,8 +1,5 @@
 const choo = require('choo')
 const html = require('bel')
-const Waveform = require('./Waveform')
-
-const wf = new Waveform('/data/cosmicosmo-twoson.mp3')
 
 const app = choo()
 
@@ -12,33 +9,52 @@ app.route('/', layout)
 app.mount('body')
 
 function layout (state, emit) {
+  const bars = 96
+  const barWidth = 12
+  const fullWidth = bars * barWidth
+
   return html`
     <body>
+
       <nav>
-        <h1>waveform</h1>
+        <label>length: </label>
+        <input type="number" min="0" value=${bars} step="4">
+        <span>bars</span>
       </nav>
-      <div></div>
-      <section></section>
-      <section></section>
-      <section>${wf.el}</section>
+
+      <div id="top-half">
+        <section id="timeline">
+
+          <div class="rail" style="min-width: ${fullWidth}px;">
+            <div class="segment-block" style="left: 240px">
+            </div>
+          </div>
+
+          <br>
+
+          <div class="rail" style="min-width: ${fullWidth}px;">
+            <div class="segment-block" style="left: 480px">
+            </div>
+          </div>
+
+        </section>
+      </div>
+
+      <div id="bottom-half">
+        <div id="segment-browser">
+          <p>segments</p>
+        </div>
+
+        <section id="waveform-editor">
+          <p>selected waveform</p>
+        </section>
+      </div>
     </body>
   `
 }
 
 function store (state, emitter) {
   emitter.on('DOMContentLoaded', function () {
-    wf.render()
+    // wf.render()
   })
 }
-
-
-
-// const Waveform = require('./Waveform')
-// const waveform = new Waveform('/data/cosmicosmo-twoson.mp3')
-
-// document.body.appendChild(waveform.el)
-// waveform.render()
-
-// const Radar = require('./Radar')
-// const radar = new Radar()
-// document.body.appendChild(radar.el)
